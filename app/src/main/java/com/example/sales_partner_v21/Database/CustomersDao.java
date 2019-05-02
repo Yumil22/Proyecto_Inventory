4074047175eaf1849c5a2fd19ad3dd44ce9dd854
AlexJMatos <47837699+AlexJMatos@users.noreply.github.com>
2019-05-02 10:39:39 -0500

    commit chido

--- app/src/main/java/com/example/sales_partner_v21/Database/CustomersDao.java
+++ app/src/main/java/com/example/sales_partner_v21/Database/CustomersDao.java
@@ -35,11 +35,7 @@ public interface CustomersDao {
     @Query("SELECT * FROM CUSTOMERS WHERE phone1 LIKE '%' || :phone || '%' OR phone2 || '%' LIKE '%' || :phone || '%'OR phone3 LIKE '%' || :phone || '%'")
     List<Customers> getCustomersbyPhone (String phone);
 
-    @Query("SELECT * FROM CUSTOMERS WHERE first_name LIKE '%' || :name  || '%' OR " +
-            "last_name LIKE '%' || :last_name || '%' OR " +
-            "address LIKE '%' || :address || '%' OR " +
-            "email LIKE '%' || :email  || '%' OR " +
-            "phone1 LIKE '%' || :phone || '%' OR phone2 || '%' LIKE '%' || :phone || '%'OR phone3 LIKE '%' || :phone || '%'")
+    @Query("SELECT * FROM CUSTOMERS WHERE first_name LIKE '%' || :name  || '%' OR last_name LIKE '%' || :last_name || '%' OR address LIKE '%' || :address || '%' OR email LIKE '%' || :email  || '%' OR phone1 LIKE '%' || :phone || '%' OR phone2 || '%' LIKE '%' || :phone || '%'OR phone3 LIKE '%' || :phone || '%'")
     List<Customers> getCustomerByAll(String name, String last_name, String address, String email, String phone);
 
     @Query("SELECT id FROM CUSTOMERS ORDER BY id DESC LIMIT 1")
@@ -74,4 +70,5 @@ public interface CustomersDao {
             " INNER JOIN products p ON p.id = ap.product_id" +
             " WHERE o.status_id = 0 GROUP BY o.id ORDER BY SUM(p.price)ASC")
     List<Integer> getIdLesscount();
+
 }
