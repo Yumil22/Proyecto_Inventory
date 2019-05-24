64d996fc0a46e5aae1f49e4d278e8082b45a599d
Antonio <50622027+eragonSpirit@users.noreply.github.com>
2019-05-23 21:31:46 -0500

    nuevo login

--- app/src/main/java/com/example/sales_partner_v21/MainActivity.java
+++ app/src/main/java/com/example/sales_partner_v21/MainActivity.java
@@ -5,14 +5,17 @@ import android.app.Dialog;
 import android.app.DownloadManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.example.sales_partner_v21.SellersLogin.SellersLogin;
 import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     ImageButton ClientsBtn;
     ImageButton AssembliesBtn;
     ImageButton InfoBtn;
+
+    Button Login, Logout;
+
+    Boolean log;
     private RequestQueue queue;
 
     public static final int PRINCIPAL_REQUEST_CODE = 1;
@@ -33,14 +40,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Stetho.initializeWithDefaults(this);
 
+        SharedPreferences configPreferences = getSharedPreferences("LOG", 0);
+        log = configPreferences.getBoolean("CODIGOLOGIN", false);
+
         productsBtn = findViewById(R.id.productsBtn);
         ReportsBtn = findViewById(R.id.ReportsBtn);
         OrdersBtn = findViewById(R.id.OrdersBtn);
         ClientsBtn = findViewById(R.id.ClientsBtn);
         AssembliesBtn = findViewById(R.id.AssembliesBtn);
         InfoBtn = findViewById(R.id.InfoBtn);
+        Login = findViewById(R.id.log_in_button);
+        Logout = findViewById(R.id.log_out_button);
 
         queue = Volley.newRequestQueue(this);
+
+        if(log==true){
+            Logout.setEnabled(true);
+            Login.setEnabled(false);
+            productsBtn.setEnabled(true);
+            ReportsBtn.setEnabled(true);
+            OrdersBtn.setEnabled(true);
+            ClientsBtn.setEnabled(true);
+            AssembliesBtn.setEnabled(true);
+        }else if (log==false){
+
+            android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Inicion de sesion");
+            builder.setMessage("Es necesario iniciar sesion para acceder a las opciones");
+            builder.setNeutralButton("Ok",null);
+            Dialog dialog = builder.create();
+            dialog.show();
+            dialog.setCancelable(false);
+
+
+            Logout.setEnabled(false);
+            Login.setEnabled(true);
+            productsBtn.setEnabled(false);
+            ReportsBtn.setEnabled(false);
+            OrdersBtn.setEnabled(false);
+            ClientsBtn.setEnabled(false);
+            AssembliesBtn.setEnabled(false);
+        }
 
         productsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,6 +146,45 @@ public class MainActivity extends AppCompatActivity {
                 }) */;
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SellersLogin.class);
+                MainActivity.super.finish();
+                startActivityForResult(intent, SellersLogin.SELLERS_REQUEST_CODE);
+            }
+        });
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Cerrar Sesion");
+                builder.setMessage("No podras acceder a ninguna opción");
+                builder.setPositiveButton("Cerrar sesion", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences newassembly = getSharedPreferences("LOG", 0);
+                        SharedPreferences.Editor asemblyEditor = newassembly.edit();
+                        asemblyEditor.putBoolean("CODIGOLOGIN", false);
+                        asemblyEditor.apply();
+                        Logout.setEnabled(false);
+                        Login.setEnabled(true);
+                        productsBtn.setEnabled(false);
+                        ReportsBtn.setEnabled(false);
+                        OrdersBtn.setEnabled(false);
+                        ClientsBtn.setEnabled(false);
+                        AssembliesBtn.setEnabled(false);
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel,null);
+                Dialog dialog = builder.create();
+                dialog.show();
+                dialog.setCancelable(false);
             }
         });
     }
--- app/src/main/java/com/example/sales_partner_v21/SellersLogin/SellersLogin.java
+++ app/src/main/java/com/example/sales_partner_v21/SellersLogin/SellersLogin.java
@@ -2,6 +2,8 @@
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sales_partner_v21.Database.Sellers;
+import com.example.sales_partner_v21.MainActivity;
+import com.example.sales_partner_v21.ProductsActivity;
 import com.example.sales_partner_v21.R;
 import com.example.sales_partner_v21.ViewModel.SellersModel;
 
@@ -22,6 +26,7 @@ import java.util.List;
 
     private EditText Getseller;
     private  EditText Getpassword;
+     public static final int SELLERS_REQUEST_CODE =1;
 
     private Editable seller;
     private Editable password;
@@ -41,7 +46,7 @@ import java.util.List;
 
         Getseller = findViewById(R.id.seller_login);
         Getpassword = findViewById(R.id.seller_password);
-        test = findViewById(R.id.button_test);
+        test = findViewById(R.id.button_log);
         detail = findViewById(R.id.details_sm);
 
         model = ViewModelProviders.of(this).get(SellersModel.class);
@@ -58,25 +63,21 @@ import java.util.List;
                             String FlagSeller =sellers.getUserName();
 
                             String FlagPassword = sellers.getPassword();
-                            if (FlagSeller.equals(seller.toString()) && FlagPassword.equals(password.toString())){
-                                model.getSellerIdAndName("%"+ seller + "%", "%" + password + "%").observe(SellersLogin.this, new Observer<Sellers>() {
-                                    @Override
-                                    public void onChanged(@Nullable Sellers sellers) {
-                                        int id = sellers.getId();
-                                        String name = sellers.getUserName() + " "+ Integer.toString(id);
-
-                                        Toast.makeText(SellersLogin.this, name , Toast.LENGTH_SHORT).show();
-
-
-                                    }
-                                });
-                            }else if (FlagPassword.equals(password.toString()) && !FlagSeller.equals(seller.toString())){
-                                detail.setText("Quers decir: " + FlagSeller);
-                            }else  if (seller.toString().isEmpty() || seller == null || password.toString().isEmpty() || password == null) {
-                                Toast.makeText(SellersLogin.this, "Ingrese Usuario o contraseña ", Toast.LENGTH_SHORT).show();
-                            }
+                           if (seller.toString().isEmpty() || seller == null || password.toString().isEmpty() || password == null){
+                               Toast.makeText(SellersLogin.this, "No se detecta el usuario o contraseña", Toast.LENGTH_SHORT).show();
+                           }else {
+                               Toast.makeText(SellersLogin.this, "Worjs!", Toast.LENGTH_SHORT).show();
+                           }
                         }
                     });
+
+                    SharedPreferences newassembly = getSharedPreferences("LOG", 0);
+                    SharedPreferences.Editor asemblyEditor = newassembly.edit();
+                    asemblyEditor.putBoolean("CODIGOLOGIN", true);
+                    asemblyEditor.apply();
+                    Intent intent = new Intent(SellersLogin.this, MainActivity.class);
+                    SellersLogin.super.finish();
+                    startActivityForResult(intent, MainActivity.PRINCIPAL_REQUEST_CODE);
                 }
             });
 
